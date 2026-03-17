6d63c96fed5281e482f7b29720eb5b2e6a916ead
Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>
2026-03-17 15:26:39 -0300

    feat: update fonts and colors
    Signed-off-by: Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { NavLink, Route, Routes } from 'react-router-dom'
-import { About } from './pages/About.tsx'
-import { Home } from './pages/Home.tsx'
-import { NotFound } from './pages/NotFound.tsx'
+import { NavLink, Route, Routes } from "react-router-dom";
+import { About } from "./pages/About.tsx";
+import { Home } from "./pages/Home.tsx";
+import { NotFound } from "./pages/NotFound.tsx";
 
 function App() {
   return (
     <>
-      <header style={{ padding: '12px 16px' }}>
-        <nav style={{ display: 'flex', gap: 12 }}>
+      <header style={{ padding: "12px 16px" }}>
+        <nav style={{ display: "flex", gap: 12 }}>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
         </nav>
@@ -19,7 +19,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
