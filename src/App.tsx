27f74310a87715f6b8f8e7ed612538cf56584b8e
Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>
2026-03-17 15:41:35 -0300

    feat: cleanup and header component
    Signed-off-by: Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { NavLink, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { About } from "./pages/About.tsx";
 import { Home } from "./pages/Home.tsx";
 import { NotFound } from "./pages/NotFound.tsx";
@@ -6,18 +8,15 @@ import { NotFound } from "./pages/NotFound.tsx";
 function App() {
   return (
     <>
-      <header style={{ padding: "12px 16px" }}>
-        <nav style={{ display: "flex", gap: 12 }}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-        </nav>
-      </header>
+      <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo-comarte.svg";
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header>
+      <NavLink to="/" className={styles.brand} aria-label="Ir para início">
+        <img className={styles.logo} src={logo} alt="Comarte" />
+      </NavLink>
+
+      <nav>
+        <NavLink to="/" end className={styles.link}>
+          início
+        </NavLink>
+        <NavLink to="/catalogo" className={styles.link}>
+          catálogo
+        </NavLink>
+        <NavLink to="/autores" className={styles.link}>
+          autores
+        </NavLink>
+        <NavLink to="/quem-somos" className={styles.link}>
+          quem somos
+        </NavLink>
+        <NavLink to="/contato" className={styles.link}>
+          contato
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
