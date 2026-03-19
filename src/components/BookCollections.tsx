62cc686347b9d071dfd881ad1eae6aee6abe5fef
Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>
2026-03-19 14:59:30 -0300

    feat: add different colors to collections
    Signed-off-by: Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>

--- src/components/BookCollections.tsx
+++ src/components/BookCollections.tsx
@@ -1,26 +1,41 @@
 import booksData from "../db/books.json";
 import styles from "./BookCollections.module.css";
 
-const collections = [...new Set(
-  booksData
-    .filter(book => book.collection.length > 0)
-    .map(book => book.collection)
-)];
+const collections = [
+  ...new Set(
+    booksData
+      .filter((book) => book.collection.length > 0)
+      .map((book) => book.collection),
+  ),
+].slice(0, 3);
 
 export function BookCollections() {
-  return <section className={styles.collections}>
-    <ul>
-      {collections.map(collection => (
-        <li key={collection}>
-          <CollectionStrip name={collection} />
-        </li>
-      ))}
-    </ul>
-  </section>;
+  return (
+    <section className={styles.collections}>
+      <ul>
+        {collections.map((collection, index) => (
+          <li key={index}>
+            <CollectionStrip
+              name={collection}
+              className={styles[`variant-${index + 1}`]}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
-export function CollectionStrip({ name }: { name: string }) {
-  return <section className={styles.collection}>
-    <h3>{name}</h3>
-  </section>;
+export function CollectionStrip({
+  name,
+  className,
+}: {
+  name: string;
+  className: string;
+}) {
+  return (
+    <section className={`${styles.collection} ${className}`}>
+      <h3>{name}</h3>
+    </section>
+  );
 }
