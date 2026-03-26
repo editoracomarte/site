e6c6ece9506107c71f2ef95c249b2b6db1d17c8b
Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>
2026-03-25 21:48:07 -0300

    fix: remove import for About page
    Signed-off-by: Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import { About } from "./pages/About.tsx";
 import { BookDetails } from "./pages/BookDetails.tsx";
 import { Home } from "./pages/Home.tsx";
 import { NotFound } from "./pages/NotFound.tsx";
