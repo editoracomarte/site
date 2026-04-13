e2e094cc4abc41c3cfee833792c858aea0e0a456
Ricardo H H Kojo <ricardo.kojo@ime.usp.br>
2026-04-13 00:21:24 -0300

    feat: add address and contact on footer
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo@ime.usp.br>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,17 @@ export function Footer() {
 
   return (
     <footer>
-      <small className={styles.text}>
+      <address>
+        Av. Prof. Lúcio Martins Rodrigues, 443 – Prédio 2 – Sala 10<br/>
+        CEP 05508-020 – Cidade Universitária – São Paulo, SP – Brasil
+      </address>
+      <p>
+        Telefone: <a href="tel:+551130914016">(11) 3091-4016</a>
+      </p>
+      <p>
+        Email: <a href="mailto:editoracomarte@usp.br">editoracomarte@usp.br</a>
+      </p>
+      <small>
         © {year} Com-Arte Editora Laboratório. Todos os direitos reservados.
       </small>
     </footer>
