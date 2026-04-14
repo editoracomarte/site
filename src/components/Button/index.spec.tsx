72e995ca1da2f9288a0c0c964247fdb610a1b35b
Ricardo H H Kojo <ricardo.kojo@ime.usp.br>
2026-04-14 19:59:39 -0300

    lint: fix lint
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo@ime.usp.br>

--- src/components/Button/index.spec.tsx
+++ src/components/Button/index.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';;
+import userEvent from '@testing-library/user-event';
 import { Button } from './index';
 
 it('calls onClick when clicked', async () => {
