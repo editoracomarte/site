8280282e0bab6c6a593c3a39a5bba7110e79b60b
Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>
2026-05-05 21:42:30 -0300

    feat: improve footer
    Signed-off-by: Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,38 @@
 import styles from './Footer.module.css';
+import logo from '@/assets/logo-comarte.svg';
 
 export function Footer() {
   const year = new Date().getFullYear();
 
   return (
     <footer className={styles.footer}>
-      <address>
-        Av. Prof. Lúcio Martins Rodrigues, 443 – Prédio 2 – Sala 10
-        <br />
-        CEP 05508-020 – Cidade Universitária – São Paulo, SP – Brasil
-        <br />
-        <a href="tel:+551130914016">(11) 3091-4016</a>
-        <br />
-        <a href="mailto:editoracomarte@usp.br">editoracomarte@usp.br</a>
-      </address>
-      <small>© {year} Com-Arte Editora Laboratório. Todos os direitos reservados.</small>
+      <section className={styles.info}>
+        <section className={styles.supporters}>
+          <span className="sr-only">Com apoio de:</span>
+          <img src={logo} alt="Departamento de Jornalismo e Editoração da ECA-USP" />
+          <img src={logo} alt="Escola de Artes e Comunicação da USP" />
+          <img src={logo} alt="Universidade de São Paulo" />
+        </section>
+        <section className={styles.address}>
+          <span className="sr-only">Endereço:</span>
+          <address>
+            Av. Prof. Lúcio Martins Rodrigues, 443 – Prédio 2 – Sala 10
+            <br />
+            CEP 05508-020 – Cidade Universitária – São Paulo, SP – Brasil
+            <br />
+            <a href="tel:+551130914016" aria-label="Telefone DDD 11 3 0 9 1 4 0 1 6">
+              (11) 3091-4016
+            </a>
+            <br />
+            <a href="mailto:editoracomarte@usp.br">
+              <span className="sr-only">Email:</span>editoracomarte@usp.br
+            </a>
+          </address>
+        </section>
+      </section>
+      <section className={styles.copyright}>
+        <small>© {year} Com-Arte Editora Laboratório. Todos os direitos reservados.</small>
+      </section>
     </footer>
   );
 }
