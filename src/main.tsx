5665ba98edf91467bd3a3b9432103905ce312324
Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>
2026-05-28 16:34:21 -0300

    feat: create ScrollToTop component
    Signed-off-by: Ricardo H. H. Kojo <ricardo.kojo@wellhub.com>

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import App from './App.tsx';
 import { queryClient } from '@/queries/queryClient.ts';
+import ScrollToTop from './components/ScrollToTop/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
